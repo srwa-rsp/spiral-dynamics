35a9af8d36b2cfb6248f11d1904c92b5c5181e93
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-03-05 13:02:19 -0500

    fix(stages):add-more-stages

--- src/pages/api/stages.ts
+++ src/pages/api/stages.ts
@@ -2,11 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Stages } from '@/pages/types/stages'; // Adjust the import path as necessary
 
 const spiralDynamicsStages: Stages[] = [
-  { id: 1, name: 'Beige', color: 'Beige', description: 'SurvivalSense - Instinctive' },
-  { id: 2, name: 'Purple', color: 'Purple', description: 'KinSpirits - Clannish' },
-  { id: 3, name: 'Red', color: 'Red', description: 'PowerGods - Egocentric' },
-  { id: 4, name: 'Blue', color: 'Blue', description: 'TruthForce - Absolutistic' },
-];
+  { id: 1, name: 'Beige', color: 'Beige', description: 'SurvivalSense - Instinctive', category: 'tier one' },
+  { id: 2, name: 'Purple', color: 'Purple', description: 'KinSpirits - Animistic', category: 'tier one' },
+  { id: 3, name: 'Red', color: 'Red', description: 'PowerGods - Egocentric', category: 'tier one' },
+  { id: 4, name: 'Blue', color: 'Blue', description: 'TruthForce - Absolutistic', category: 'tier one' },
+  { id: 5, name: 'Orange', color: 'Orange', description: 'StriveDrive - Multiplistic', category: 'tier one' },
+  { id: 6, name: 'Green', color: 'Green', description: 'HumanBond - Relativistic', category: 'tier one' },
+  { id: 7, name: 'Yellow', color: 'Yellow', description: 'FlexFlow - Systemic', category: 'tier two' },
+  { id: 8, name: 'Turquoise', color: 'Turquoise', description: 'GlobalView - Holistic', category: 'tier two' },
+  { id: 9, name: 'Coral', color: 'Coral', description: 'WholeView - Holonic', category: 'tier two' }
+]
 
 export default function handler(
   req: NextApiRequest,
--- src/pages/types/stages.ts
+++ src/pages/types/stages.ts
@@ -3,4 +3,5 @@ export type Stages = {
     name: string;
     color: string;
     description: string;
+    category: string
   };
